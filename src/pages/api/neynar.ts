e25b74c279bd46b1d14eb61299249766f883638a
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-06-30 17:09:06 +0200

    setup neynar post request

---
+++ src/pages/api/neynar.ts
@@ -0,0 +1,18 @@
+const options = {
+  method: "POST",
+  headers: {
+    accept: "application/json",
+    api_key: process.env.NEYNAR_API_KEY || "",
+    "content-type": "application/json",
+  },
+  body: JSON.stringify({
+    parent_author_fid: 3,
+    text: "test cast qcast",
+    signer_uuid: process.env.SIGNER_UUID,
+  }),
+};
+
+fetch("https://api.neynar.com/v2/farcaster/cast", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
