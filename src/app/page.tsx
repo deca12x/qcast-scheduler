cb3c2fc6dc921c2c134b43faa1f8004ba393865d
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-06-30 17:45:37 +0200

    can connect to my farcaster profile in frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { NeynarAuthButton, useNeynarContext } from "@neynar/react";
 import Image from "next/image";
 
 export default function Home() {
@@ -40,6 +43,7 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+        <NeynarAuthButton />
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
